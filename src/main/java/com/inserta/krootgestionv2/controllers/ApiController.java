d7886db904d0da94c4a421b2589e448410517ad3
MiguelAngelHerreraGarcia <miguelherreragarcia@outlook.es>
2022-05-12 13:28:56 +0200

    localdatetime que sea lo que dios quiera

--- src/main/java/com/inserta/krootgestionv2/controllers/ApiController.java
+++ src/main/java/com/inserta/krootgestionv2/controllers/ApiController.java
@@ -1,10 +1,7 @@
 package com.inserta.krootgestionv2.controllers;
 
 import com.inserta.krootgestionv2.models.*;
-import com.inserta.krootgestionv2.repos.ActividadesRepo;
-
-import com.inserta.krootgestionv2.repos.SociosRepo;
-import com.inserta.krootgestionv2.repos.UsuariosRepo;
+import com.inserta.krootgestionv2.services.ActividadesService;
 import com.inserta.krootgestionv2.services.ComunicadosServices;
 import com.inserta.krootgestionv2.services.EncuestasService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -21,42 +19,31 @@ import java.util.List;
 public class ApiController {
 
     @Autowired
-    private ActividadesRepo actividadesRepo;
-
-    @Autowired
-    private SociosRepo sociosRepo;
-
-    @Autowired
-    private UsuariosRepo usuariosRepo;
-
-    @Autowired
     private EncuestasService encuestasService;
 
     @Autowired
     private ComunicadosServices comunicadosService;
 
-    @GetMapping("/actividades")
-    public List<Actividad> getActividad() {
-        return actividadesRepo.findAll();
-    }
+    @Autowired
+    private ActividadesService actividadesService;
 
-    @GetMapping("/usuarios")
-    public List<Usuario> getUsuarios() {
-        return usuariosRepo.findAll();
-    }
-
-    @GetMapping("/socios")
-    public List<Socio> getSocios(){
-        return sociosRepo.findAll();
-    }
+    @Autowired
+    private SociosService sociosService;
 
     @GetMapping("/encuestas/{nombre}")
     public List<Encuesta> getEncuestas(@PathVariable String nombre){
         return encuestasService.findByNombre(nombre);
     }
-
     @GetMapping("/comunicados/actividad/{id}")
     public List<Comunicado> getComunicados(@PathVariable int id){
         return comunicadosService.getComunicadosByIdActividad(id);
+    }
+    @GetMapping("/actividades/tipo/{id}")
+    public List<Actividad> getActividadesByTipo(@PathVariable int id){
+        return actividadesService.getActividadesByTipo(id);
+    }
+    @GetMapping("/socios/fechaacceso/{fecha}")
+    public List<Socio> getSociosByFecha(@PathVariable LocalDateTime fecha){
+        return sociosService.getSociosByFechaUltimoAcceso(fecha);
     }
 }
