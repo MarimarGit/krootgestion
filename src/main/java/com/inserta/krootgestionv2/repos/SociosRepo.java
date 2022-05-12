400ac959cad9331fe7598dbee82d3cc9e8d32318
apemarr <apematica@gmail.com>
2022-05-12 12:26:18 +0100

    Agregado método obtener socios por último acceso

--- src/main/java/com/inserta/krootgestionv2/repos/SociosRepo.java
+++ src/main/java/com/inserta/krootgestionv2/repos/SociosRepo.java
@@ -5,8 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 @RepositoryRestResource(path = "socios")
 public interface SociosRepo extends JpaRepository<Socio, Integer> {
 
+    /**
+     * Encuentra una lista de socios por fecha de último acceso
+     * @param fecha La fecha de último acceso
+     * @return List de socios
+     */
+    List<Socio> findByFechaUltimoAcceso(LocalDateTime fecha);
 }
--- src/main/java/com/inserta/krootgestionv2/services/SociosService.java
+++ src/main/java/com/inserta/krootgestionv2/services/SociosService.java
@@ -1,5 +1,10 @@
 package com.inserta.krootgestionv2.services;
 
-public interface SociosService {
+import com.inserta.krootgestionv2.models.Socio;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface SociosService {
+    public List<Socio> getSociosByFechaUltimoAcceso(LocalDateTime fecha);
 }
