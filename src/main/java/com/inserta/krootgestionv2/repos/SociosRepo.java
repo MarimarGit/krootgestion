36bc43eeaebc1bb81285fcfde7026a931048370a
apemarr <apematica@gmail.com>
2022-05-14 21:33:21 +0100

    Arreglada búsqueda por fecha de último acceso en SociosRepo

--- src/main/java/com/inserta/krootgestionv2/repos/SociosRepo.java
+++ src/main/java/com/inserta/krootgestionv2/repos/SociosRepo.java
@@ -3,6 +3,7 @@ package com.inserta.krootgestionv2.repos;
 import com.inserta.krootgestionv2.models.Socio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -17,5 +18,5 @@ public interface SociosRepo extends JpaRepository<Socio, Integer> {
      * @param fecha La fecha de último acceso
      * @return List de socios
      */
-    List<Socio> findByFechaUltimoAcceso(LocalDateTime fecha);
+    List<Socio> findByFechaUltimoAcceso(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fecha);
 }
